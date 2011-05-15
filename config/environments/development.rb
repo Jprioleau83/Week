4e8535470764fa3fc5c9702b494e42ceeffa2c94
Johnny A. Prioleau <jprioleau83@gmail.com>
2011-05-14 23:39:59 -0400

    added config env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,6 +23,6 @@ Week::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
   
-  config.action_mailer.default_url_options = { :host => 'stormy-leaf-635.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'gentle-summer-141.heroku.com' }
 end
 
