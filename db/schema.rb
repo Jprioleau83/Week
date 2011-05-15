33ee886d896a0f842c94e7abed3e90ff813e86bb
Johnny A. Prioleau <jprioleau83@gmail.com>
2011-05-14 22:48:44 -0400

    make user confirmable schema

--- db/schema.rb
+++ db/schema.rb
@@ -44,10 +44,13 @@ ActiveRecord::Schema.define(:version => 20110514234724) do
   add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "",   :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "",   :null => false
-    t.string   "password_salt",                       :default => "",   :null => false
-    t.boolean  "admin",                               :default => true
+    t.string   "email",                               :default => "",    :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                       :default => "",    :null => false
+    t.boolean  "admin",                               :default => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
@@ -61,6 +64,7 @@ ActiveRecord::Schema.define(:version => 20110514234724) do
     t.string   "name"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
